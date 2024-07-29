82f807a24bfbede0d9dc4ece95363e3be2801a97
linda-row <lindaxxxrow@gmail.com>
2024-07-29 09:12:57 +0900

    deleted:this commit should remove wrong projects

--- CryptoWallet/src/core/blockchain/credentials/protocols/ERC20.js
+++
@@ -1,9 +0,0 @@
-import { provideAddress as provideAddressETH, providePrivateKey as providePrivateKeyETH } from './ETH';
-
-export async function provideAddress(queue, mnemonic) {
-    return await provideAddressETH(queue, mnemonic);
-}
-
-export async function providePrivateKey(queue, mnemonic) {
-    return await providePrivateKeyETH(queue, mnemonic);
-}
